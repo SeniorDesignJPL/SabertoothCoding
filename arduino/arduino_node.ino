d4d103673e6cddcc2ea3af31b9441289d9d491c9
Ryan Hodge <ryanhodge240@gmail.com>
2023-03-09 23:14:54 +0000

    Arduino code working

--- arduino/arduino_node.ino
+++ arduino/arduino_node.ino
@@ -12,6 +12,8 @@
 SoftwareSerial SWSerial(NOT_A_PIN, 18); // RX on no pin (unused), TX on pin 11 (to S1).
 USBSabertoothSerial C(SWSerial); // Use SWSerial as the serial port.
 USBSabertooth ST[3] = {USBSabertooth(C, 128), USBSabertooth(C, 129), USBSabertooth(C, 130)};
+int motor_speeds[] = {0, 0, 0, 0, 0, 0};
+int prev_speeds[] = {0, 0, 0, 0, 0, 0};
 
 ros::NodeHandle nh;
 
@@ -21,8 +23,8 @@ void messageCb (const motor_controller::Motor& msg) {
 		", Motor: " + String((msg.address % 2) + 1) +
 		", Speed: " + String(msg.speed)).c_str()
 	);
-
-	ST[msg.address / 2].motor((msg.address % 2) + 1, msg.speed);
+	motor_speeds[msg.address] = msg.speed;
+	// ST[msg.address / 2].motor((msg.address % 2) + 1, msg.speed);
 }
 
 ros::Subscriber<motor_controller::Motor> sub("arduino/motors", &messageCb);
@@ -34,6 +36,14 @@ void setup() {
 }
 
 void loop() {
+	if (prev_speeds[0] != motor_speeds[0]) {
+		ST[0].motor(1, motor_speeds[0]);
+		prev_speeds[0] = motor_speeds[0];
+	}
+	if (prev_speeds[2] != motor_speeds[2]) {
+		ST[1].motor(1, motor_speeds[2]);
+		prev_speeds[2] = motor_speeds[2];
+	}
 	nh.spinOnce();
 	delay(1);
 }
