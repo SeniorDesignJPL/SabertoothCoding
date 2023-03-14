8125edfc129b25d08b8cccb92695148242522fab
Ryan Hodge <ryanhodge240@gmail.com>
2023-03-14 23:58:26 +0000

    Added kangaroo ROS stuff to another file

--- arduino/arduino_kangaroo_ros.ino
+++ arduino/arduino_kangaroo_ros.ino
@@ -4,6 +4,9 @@
 
 #include <Kangaroo.h>
 #include "Sabertooth.h"
+#include <ros.h>
+#include <std_msgs/Int32MultiArray.h>
+#include <motor_controller/Motor.h>
 
 // Arduino TX (pin 11) goes to Kangaroo S1
 // Arduino RX (pin 10) goes to Kangaroo S2
@@ -14,12 +17,28 @@
 KangarooSerial  K(Serial1);
 KangarooChannel K1(K, '1', 128);
 KangarooChannel K2(K, '2', 128);
+// KangarooChannel K_MOTORS[2] = { KangarooChannel(K, '1', 128), KangarooChannel(K, '2', 128) }
+
+int motor_speeds[] = {0, 0, 0, 0, 0, 0};
+int prev_speeds[] = {0, 0, 0, 0, 0, 0};
+
+ros::NodeHandle nh;
+
+void messageCb (const motor_controller::Motor& msg) {
+	motor_speeds[msg.address] = msg.speed;
+}
+
+ros::Subscriber<motor_controller::Motor> sub("arduino/motors", &messageCb);
 
 void setup() {
+	nh.initNode();
+	nh.subscribe(sub);
 	Serial1.begin(9600);
 	
 	K1.start();
+	K2.start();
 	K1.home().wait();
+	K2.home().wait();
 }
 
 // .wait() waits until the command is 'finished'. For speed, this means it reached near the
@@ -27,10 +46,14 @@ void setup() {
 // but not wait to get up to speed. If you do this, you may want to use K1.getS().value()
 // to check progress.
 void loop() {
-	int speed = 200; 
-	K1.s(speed);
-	delay(5000);
-	
-	K1.s(-speed);
-	delay(5000);
+	if (prev_speeds[0] != motor_speeds[0]) {
+		K1.s(motor_speeds[0]);
+		prev_speeds[0] = motor_speeds[0];
+	}
+	if (prev_speeds[1] != motor_speeds[1]) {
+		K2.s(motor_speeds[1]);
+		prev_speeds[1] = motor_speeds[1];
+	}
+	nh.spinOnce();
+	delay(1);
 }
